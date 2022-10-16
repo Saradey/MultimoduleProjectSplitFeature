08730280615671ed3d2f8c4a8dc61c5499919d41
Evgeny Goncharov <jtgn@yandex.ru>
2022-10-16 16:16:44 +0300

    release di engine

--- app/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/App.kt
+++ app/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/App.kt
@@ -1,6 +1,33 @@
 package com.evgeny.goncharov.sample.multimodule
 
 import android.app.Application
+import com.evgeny.goncharov.sample.multimodule.di_core.container.FeatureContainerImpl
+import com.evgeny.goncharov.sample.multimodule.di_core.engine.DI
+import com.evgeny.goncharov.sample.multimodule.initializer.FeatureHolderInitializer
+import com.evgeny.goncharov.sample.multimodule.initializer.GlobalHolderInitializer
+import java.lang.Exception
 
 internal class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initDi()
+    }
+
+    private fun initDi() {
+        try {
+            val featureContainer = FeatureContainerImpl()
+            featureContainer.init(
+                GlobalHolderInitializer,
+                FeatureHolderInitializer
+            )
+            DI.initialize(featureContainer)
+        } catch (exp: Exception) {
+            throw RuntimeException("$FAILED_INIT_DI_MESSAGE $exp")
+        }
+    }
+
+    companion object {
+        private const val FAILED_INIT_DI_MESSAGE = "Fail to init DI"
+    }
 }
