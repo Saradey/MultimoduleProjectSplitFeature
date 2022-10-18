afaf696557c902cf9b50b250bd6a9fecda16e8d7
Evgeny Goncharov <jtgn@yandex.ru>
2022-10-18 18:15:09 +0300

    added launchers to dagger

--- app/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/di/modules/GlobalHoldersModule.kt
+++ app/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/di/modules/GlobalHoldersModule.kt
@@ -1,8 +1,10 @@
 package com.evgeny.goncharov.sample.multimodule.di.modules
 
+import com.evgeny.goncharov.sample.multimodule.di.holder.LaunchersHolder
 import com.evgeny.goncharov.sample.multimodule.di_core.container.FeatureContainerManager
 import com.evgeny.goncharov.sample.multimodule.di_core.holder.BaseHolder
 import com.evgeny.goncharov.sample.multimodule.navigation.api.CoreNavigationApi
+import com.evgeny.goncharov.sample.multimodule.navigation.api.LaunchersApi
 import com.evgeny.goncharov.sample.multimodule.navigation.holder.NavigationHolder
 import dagger.Module
 import dagger.Provides
@@ -17,4 +19,9 @@ internal object GlobalHoldersModule {
     @[IntoMap ClassKey(CoreNavigationApi::class)]
     fun provideNavigationHolder(featureContainer: FeatureContainerManager): BaseHolder<*> =
         NavigationHolder(featureContainer)
+
+    @[Provides Singleton]
+    @[IntoMap ClassKey(LaunchersApi::class)]
+    fun provideLaunchersHolder(featureContainer: FeatureContainerManager): BaseHolder<*> =
+        LaunchersHolder(featureContainer)
 }
