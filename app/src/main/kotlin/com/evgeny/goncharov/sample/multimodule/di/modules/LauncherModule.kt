c4b54e18526833eba351a2cebcc7a813d048220b
Evgeny Goncharov <jtgn@yandex.ru>
2022-10-18 18:39:21 +0300

    added splash launcher to dagger

--- app/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/di/modules/LauncherModule.kt
+++ app/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/di/modules/LauncherModule.kt
@@ -1,7 +1,13 @@
 package com.evgeny.goncharov.sample.multimodule.di.modules
 
+import com.evgeny.goncharov.sample.multimodule.splash_screen_api.SplashScreenLauncher
+import com.evgeny.goncharov.sample.multimodule.splash_screen_impl.navigation.SplashScreenLauncherImpl
+import dagger.Binds
 import dagger.Module
 
 @Module
-internal object LauncherModule {
+internal interface LauncherModule {
+
+    @Binds
+    fun SplashScreenLauncherImpl.bindSplashScreenLauncher(): SplashScreenLauncher
 }
