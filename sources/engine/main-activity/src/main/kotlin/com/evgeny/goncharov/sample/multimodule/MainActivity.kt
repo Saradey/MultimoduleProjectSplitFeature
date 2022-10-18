0f0b40bf352e8936c26ef7b71a73f71f628ab27a
Evgeny Goncharov <jtgn@yandex.ru>
2022-10-18 20:02:06 +0300

    added start logic code to main activity

--- sources/engine/main-activity/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/MainActivity.kt
+++ sources/engine/main-activity/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/MainActivity.kt
@@ -4,16 +4,29 @@ import android.os.Bundle
 import com.evgeny.goncharov.sample.multimodule.di.contracts.MainActivityApi
 import com.evgeny.goncharov.sample.multimodule.di.contracts.MainActivityInternal
 import com.evgeny.goncharov.sample.multimodule.di_core.CoreActivity
+import com.github.terrakok.cicerone.androidx.AppNavigator
 
 public class MainActivity : CoreActivity() {
 
     private val dependency: MainActivityInternal by lazy {
         getFeatureApi(MainActivityApi::class.java) as MainActivityInternal
     }
+    private val navigator = AppNavigator(this, R.id.container)
+    private val navigatorHolder = dependency.provideGlobalNavigatorHolder()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        navigatorHolder.setNavigator(navigator)
+    }
+
+    override fun onPause() {
+        navigatorHolder.removeNavigator()
+        super.onPause()
     }
 
     override fun releaseDependency() {
