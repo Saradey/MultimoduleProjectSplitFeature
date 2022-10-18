c370848b217fc48f9c530733f875a1c36b4dd41d
Evgeny Goncharov <jtgn@yandex.ru>
2022-10-18 18:50:57 +0300

    fix contract

--- app/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/di/holder/LaunchersHolder.kt
+++ app/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/di/holder/LaunchersHolder.kt
@@ -1,16 +1,16 @@
 package com.evgeny.goncharov.sample.multimodule.di.holder
 
 import com.evgeny.goncharov.sample.multimodule.di.components.DaggerLaunchersComponent
-import com.evgeny.goncharov.sample.multimodule.di.components.LaunchersComponent
 import com.evgeny.goncharov.sample.multimodule.di_core.container.FeatureContainerManager
 import com.evgeny.goncharov.sample.multimodule.di_core.holder.BaseHolder
 import com.evgeny.goncharov.sample.multimodule.navigation.api.CoreNavigationApi
+import com.evgeny.goncharov.sample.multimodule.navigation.api.LaunchersApi
 
 internal class LaunchersHolder(
     container: FeatureContainerManager
-) : BaseHolder<LaunchersComponent>(container) {
+) : BaseHolder<LaunchersApi>(container) {
 
-    override fun buildComponent(): LaunchersComponent {
+    override fun buildComponent(): LaunchersApi {
         return DaggerLaunchersComponent.builder()
             .coreNavigationApi(getGlobalComponent(CoreNavigationApi::class.java))
             .build()
