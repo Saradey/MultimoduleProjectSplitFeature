c2688d607bd5d26a44adc62eb052a871ccf351b4
Evgeny Goncharov <jtgn@yandex.ru>
2022-10-19 12:28:22 +0300

    theme logic ui sandbox

--- ui-sandbox/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/ui_sandbox/UiSandboxActivity.kt
+++ ui-sandbox/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/ui_sandbox/UiSandboxActivity.kt
@@ -2,10 +2,20 @@ package com.evgeny.goncharov.sample.multimodule.ui_sandbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.IdRes
 
 internal class UiSandboxActivity : AppCompatActivity() {
+
+    @IdRes
+    private var themeNow: Int = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ui_sandbox)
+    }
+
+    override fun setTheme(@IdRes resId: Int) {
+        super.setTheme(resId)
+        themeNow = resId
     }
 }
