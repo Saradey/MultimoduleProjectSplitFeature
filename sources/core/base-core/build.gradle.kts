74be595879f3306cc07cede12b128df696eeb090
Evgeny Goncharov <jtgn@yandex.ru>
2022-10-20 11:05:44 +0300

    added base core module

---
+++ sources/core/base-core/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = rootProject.ext["compileSdkVersionApp"] as Int
+    defaultConfig {
+        minSdk = rootProject.ext["minSdkVersionApp"] as Int
+        targetSdk = rootProject.ext["targetSdkVersionApp"] as Int
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-Xexplicit-api=strict")
+    }
+}
+
+dependencies {
+}
