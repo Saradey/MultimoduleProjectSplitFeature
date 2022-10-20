da7b4f9b85f3c58adcee84b95835847e22555bab
Evgeny Goncharov <jtgn@yandex.ru>
2022-10-20 21:19:20 +0300

    added empty container splash

--- sources/features/splash-screen-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/splash_screen_impl/SplashScreenContainerFragment.kt
+++ sources/features/splash-screen-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/splash_screen_impl/SplashScreenContainerFragment.kt
@@ -1,4 +1,17 @@
 package com.evgeny.goncharov.sample.multimodule.splash_screen_impl
 
-internal class SplashScreenContainerFragment {
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import com.evgeny.goncharov.sample.multimodule.di_core.ContainerFeatureFragment
+
+internal class SplashScreenContainerFragment : ContainerFeatureFragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+
+    override fun releaseDependencies() {
+
+    }
 }
