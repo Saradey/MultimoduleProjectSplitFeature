df02b6de6ff15ea748776f1ececa85c0aa1dd979
Evgeny Goncharov <jtgn@yandex.ru>
2022-10-20 18:56:55 +0300

    added text view accent style

--- ui-sandbox/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/ui_sandbox/UiSandboxActivity.kt
+++ ui-sandbox/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/ui_sandbox/UiSandboxActivity.kt
@@ -3,8 +3,6 @@ package com.evgeny.goncharov.sample.multimodule.ui_sandbox
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.annotation.IdRes
-import androidx.lifecycle.ViewModelProvider
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgeny.goncharov.sample.multimodule.ui_sandbox.databinding.ActivityUiSandboxBinding
 
