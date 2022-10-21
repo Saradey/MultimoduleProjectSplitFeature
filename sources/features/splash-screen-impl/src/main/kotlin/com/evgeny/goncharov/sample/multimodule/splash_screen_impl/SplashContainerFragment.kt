733b83647c08499cd69b36d453088369483e0055
Evgeny Goncharov <jtgn@yandex.ru>
2022-10-21 21:02:49 +0300

    fix

--- sources/features/splash-screen-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/splash_screen_impl/SplashContainerFragment.kt
+++ sources/features/splash-screen-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/splash_screen_impl/SplashContainerFragment.kt
@@ -3,6 +3,7 @@ package com.evgeny.goncharov.sample.multimodule.splash_screen_impl
 import android.os.Bundle
 import android.view.View
 import com.evgeny.goncharov.sample.multimodule.di_core.ContainerFeatureFragment
+import com.evgeny.goncharov.sample.multimodule.splash_screen_api.SplashApi
 
 internal class SplashContainerFragment : ContainerFeatureFragment() {
 
@@ -10,6 +11,7 @@ internal class SplashContainerFragment : ContainerFeatureFragment() {
     }
 
     override fun releaseDependencies() {
+        releaseFeatureApi(SplashApi::class.java)
     }
 
     companion object {
