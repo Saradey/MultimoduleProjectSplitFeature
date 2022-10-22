4651056f1cc6f8999358676b668c533ceaa43c52
Evgeny Goncharov <jtgn@yandex.ru>
2022-10-22 12:46:56 +0300

    added bottom impl module

--- sources/features/bottom-menu-api/build.gradle.kts
+++ sources/features/bottom-menu-api/build.gradle.kts
@@ -5,24 +5,10 @@ plugins {
 
 android {
     namespace = "com.evgeny.goncharov.sample.multimodule.bottom_menu_api"
-    compileSdk = 32
-
+    compileSdk = rootProject.ext["compileSdkVersionApp"] as Int
     defaultConfig {
-        minSdk = 23
-        targetSdk = 32
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+        minSdk = rootProject.ext["minSdkVersionApp"] as Int
+        targetSdk = rootProject.ext["targetSdkVersionApp"] as Int
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -30,15 +16,12 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-Xexplicit-api=strict")
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.7.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
