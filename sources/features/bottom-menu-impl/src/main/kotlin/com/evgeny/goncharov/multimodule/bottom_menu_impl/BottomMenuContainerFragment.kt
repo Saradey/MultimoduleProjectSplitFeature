e0ebcd78f205f276d4a4c199e56f4f53ba7bc4d1
Evgeny Goncharov <jtgn@yandex.ru>
2022-10-23 18:15:53 +0300

    added BottomNavigationView

--- sources/features/bottom-menu-impl/src/main/kotlin/com/evgeny/goncharov/multimodule/bottom_menu_impl/BottomMenuContainerFragment.kt
+++ sources/features/bottom-menu-impl/src/main/kotlin/com/evgeny/goncharov/multimodule/bottom_menu_impl/BottomMenuContainerFragment.kt
@@ -2,7 +2,8 @@ package com.evgeny.goncharov.multimodule.bottom_menu_impl
 
 import com.evgeny.goncharov.sample.multimodule.di_core.ContainerFeatureFragment
 
-internal class BottomMenuContainerFragment : ContainerFeatureFragment() {
+internal class BottomMenuContainerFragment :
+    ContainerFeatureFragment(R.layout.fragment_bottom_menu) {
 
     override fun releaseDependencies() {
 
