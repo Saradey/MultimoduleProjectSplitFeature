d8f8087e22ad115b3d9f2afab4b5cc66930bc00a
Evgeny Goncharov <jtgn@yandex.ru>
2022-10-23 19:01:36 +0300

    added style bottomMenu

--- sources/features/bottom-menu-impl/build.gradle.kts
+++ sources/features/bottom-menu-impl/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation(project(":sources:engine:navigation"))
     implementation(project(":sources:engine:di-core"))
     implementation(project(":sources:resources:ui-kit"))
+    implementation(project(":sources:resources:theme"))
     implementation(project(":sources:core:base-core"))
 
     implementation(coreLibs.viewmodel)
