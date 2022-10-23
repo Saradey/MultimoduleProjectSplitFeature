e674377ceae5bbb52947a29c60ea1e3fc99713b9
Evgeny Goncharov <jtgn@yandex.ru>
2022-10-23 21:30:02 +0300

    added api module contact

---
+++ sources/features/catalog-api/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/catalog_api/CatalogApi.kt
@@ -0,0 +1,5 @@
+package com.evgeny.goncharov.sample.multimodule.catalog_api
+
+import com.evgeny.goncharov.sample.multimodule.di_core.contracts.ReleasableApi
+
+public interface CatalogApi : ReleasableApi
---
+++ sources/features/catalog-api/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/catalog_api/CatalogLauncher.kt
@@ -0,0 +1,8 @@
+package com.evgeny.goncharov.sample.multimodule.catalog_api
+
+import com.evgeny.goncharov.sample.multimodule.di_core.navigation.BaseLauncher
+
+public interface CatalogLauncher : BaseLauncher {
+
+    public fun launcher()
+}
