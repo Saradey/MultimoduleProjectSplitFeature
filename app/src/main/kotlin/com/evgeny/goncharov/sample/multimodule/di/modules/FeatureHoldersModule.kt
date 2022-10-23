ef70d45aa8592de63e460a2bd9c810f7cb47088c
Evgeny Goncharov <jtgn@yandex.ru>
2022-10-23 22:20:52 +0300

    fix crash

--- app/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/di/modules/FeatureHoldersModule.kt
+++ app/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/di/modules/FeatureHoldersModule.kt
@@ -9,6 +9,7 @@ import com.evgeny.goncharov.sample.multimodule.di.holder.MainActivityHolder
 import com.evgeny.goncharov.sample.multimodule.di_core.container.FeatureContainer
 import com.evgeny.goncharov.sample.multimodule.di_core.holder.FeatureHolder
 import com.evgeny.goncharov.sample.multimodule.home_api.HomeApi
+import com.evgeny.goncharov.sample.multimodule.home_impl.di.holder.HomeHolder
 import com.evgeny.goncharov.sample.multimodule.registration_api.RegistrationApi
 import com.evgeny.goncharov.sample.multimodule.registration_impl.di.holder.RegistrationHolder
 import com.evgeny.goncharov.sample.multimodule.splash_screen_api.SplashApi
@@ -40,7 +41,7 @@ internal object FeatureHoldersModule {
     @[Singleton Provides]
     @[IntoMap ClassKey(HomeApi::class)]
     fun provideHomeHolder(featureContainer: FeatureContainer)
-            : FeatureHolder<*> = RegistrationHolder(featureContainer)
+            : FeatureHolder<*> = HomeHolder(featureContainer)
 
     @[Singleton Provides]
     @[IntoMap ClassKey(CatalogApi::class)]
