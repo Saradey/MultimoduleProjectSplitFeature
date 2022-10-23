f0ed08bae988129c051c0ced44c823695297df34
Evgeny Goncharov <jtgn@yandex.ru>
2022-10-23 18:56:48 +0300

    init component

--- sources/features/bottom-menu-impl/src/main/kotlin/com/evgeny/goncharov/multimodule/bottom_menu_impl/di/holders/BottomMenuHolder.kt
+++ sources/features/bottom-menu-impl/src/main/kotlin/com/evgeny/goncharov/multimodule/bottom_menu_impl/di/holders/BottomMenuHolder.kt
@@ -4,11 +4,13 @@ import com.evgeny.goncharov.multimodule.bottom_menu_impl.di.DaggerBottomMenuComp
 import com.evgeny.goncharov.sample.multimodule.bottom_menu_api.BottomMenuApi
 import com.evgeny.goncharov.sample.multimodule.di_core.container.FeatureContainer
 import com.evgeny.goncharov.sample.multimodule.di_core.holder.FeatureHolder
+import com.evgeny.goncharov.sample.multimodule.navigation.api.CoreNavigationApi
 
 public class BottomMenuHolder(container: FeatureContainer) :
     FeatureHolder<BottomMenuApi>(container) {
 
     override fun buildComponent(): BottomMenuApi {
-        TODO("Not yet implemented")
+        return DaggerBottomMenuComponent.factory()
+            .create(getGlobalComponent(CoreNavigationApi::class.java))
     }
 }
