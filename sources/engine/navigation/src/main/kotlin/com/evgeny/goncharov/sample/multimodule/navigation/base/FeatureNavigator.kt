ee13d00805cf70f6d9b895192c6de936dd40ed72
Evgeny Goncharov <jtgn@yandex.ru>
2023-01-29 23:09:18 +0300

    added errorOnApplyCommand in FeatureNavigator

--- sources/engine/navigation/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/navigation/base/FeatureNavigator.kt
+++ sources/engine/navigation/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/navigation/base/FeatureNavigator.kt
@@ -5,7 +5,7 @@ import com.evgeny.goncharov.sample.multimodule.di_core.ContainerFeatureFragment
 import com.github.terrakok.cicerone.*
 
 public class FeatureNavigator(
-    containerFragment: ContainerFeatureFragment,
+    private val containerFragment: ContainerFeatureFragment,
     private val containerId: Int = com.evgeny.goncharov.sample.multimodule.di_core.R.id.frm_feature_container,
 ) : Navigator {
 
@@ -17,9 +17,25 @@ public class FeatureNavigator(
         fm.executePendingTransactions()
     }
 
+
+    private fun errorOnApplyCommand(
+        command: Command,
+        error: RuntimeException
+    ) {
+        error(
+            ERROR_MESSAGE.format(
+                containerFragment.javaClass.canonicalName,
+                TAG,
+                command.toString(),
+                error.message
+            )
+        )
+    }
+
     private companion object {
 
         const val TAG = "FeatureNavigator"
-        const val ERROR_MESSAGE = "errorOnApplyCommand host: %s tag: %s command: %s error message: %s"
+        const val ERROR_MESSAGE =
+            "errorOnApplyCommand host: %s tag: %s command: %s error message: %s"
     }
 }
