4563465161a57f4285eb8f21e35410a47dafd0df
Evgeny Goncharov <jtgn@yandex.ru>
2023-01-29 20:46:44 +0300

    added error message to errorOnApplyCommand

--- sources/engine/navigation/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/navigation/base/MainActivityNavigator.kt
+++ sources/engine/navigation/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/navigation/base/MainActivityNavigator.kt
@@ -14,5 +14,29 @@ public class MainActivityNavigator(
 
     override fun applyCommands(commands: Array<out Command>) {
         fm.executePendingTransactions()
+        for (command in commands) {
+            try {
+                applyCommand(command)
+            } catch (e: RuntimeException) {
+                errorOnApplyCommand(command, e)
+            }
+        }
+    }
+
+    private fun applyCommand(command: Command) {
+
+    }
+
+    private fun errorOnApplyCommand(
+        command: Command,
+        error: RuntimeException
+    ) {
+        throw error(ERROR_MESSAGE.format(TAG, command.toString(), error.message))
+    }
+
+    private companion object {
+
+        const val TAG = "MainActivityNavigator"
+        const val ERROR_MESSAGE = "errorOnApplyCommand tag: %s command: %s error message: %s"
     }
 }
