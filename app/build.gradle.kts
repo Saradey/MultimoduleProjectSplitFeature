573e4045d71d63c8f6570a1c56b59cd17d8e9a3c
Evgeny Goncharov <jtgn@yandex.ru>
2023-02-16 21:50:56 +0300

    refactor app gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 }
 
 android {
+    namespace = "com.evgeny.goncharov.sample.multimodule"
     compileSdk = rootProject.ext["compileSdkVersionApp"] as Int
     defaultConfig {
         applicationId = "com.evgeny.goncharov.sample.multimodule"
