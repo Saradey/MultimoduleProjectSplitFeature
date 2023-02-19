2e35e211fe9dbbd09a63d92b3f0ba24927e00c4d
Evgeny Goncharov <jtgn@yandex.ru>
2023-02-19 16:44:21 +0300

    Revert "refactor di core"
    This reverts commit 6cac50e66457352baf1c0057be4bbc145e76ccde.

--- sources/engine/di-core/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/di_core/ContainerFeatureFragment.kt
+++ sources/engine/di-core/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/di_core/ContainerFeatureFragment.kt
@@ -5,27 +5,24 @@ import androidx.fragment.app.Fragment
 import com.evgeny.goncharov.sample.multimodule.di_core.contracts.ReleasableApi
 import com.evgeny.goncharov.sample.multimodule.di_core.engine.DI
 
-public abstract class ContainerFeatureFragment <D : ReleasableApi>: Fragment {
+public abstract class ContainerFeatureFragment : Fragment {
 
     public constructor(@LayoutRes layoutId: Int) : super(layoutId)
     public constructor() : super(R.layout.fragment_feature_container)
 
-    protected abstract val backStackName: String
-    protected abstract val dependency: D
+    public abstract val backStackName: String
 
     override fun onDestroy() {
         super.onDestroy()
         releaseDependencies()
     }
 
-    private fun releaseDependencies() {
-        releaseFeatureApi(dependency::class.java)
-    }
+    protected abstract fun releaseDependencies()
 
     protected fun getFeatureApi(key: Class<out ReleasableApi>): ReleasableApi =
         DI.getFeatureApi(key)
 
-    private fun releaseFeatureApi(key: Class<out ReleasableApi>) {
+    protected fun releaseFeatureApi(key: Class<out ReleasableApi>) {
         DI.releaseFeatureApi(key)
     }
 }
