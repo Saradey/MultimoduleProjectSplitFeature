eb587ce282d20ec4b740bd5e312a23e64026c4ab
Evgeny Goncharov <jtgn@yandex.ru>
2023-02-19 17:54:42 +0300

    added logic to forward BottomMenuNavigator

--- sources/features/bottom-menu-impl/src/main/kotlin/com/evgeny/goncharov/multimodule/bottom_menu_impl/navigation/BottomMenuNavigator.kt
+++ sources/features/bottom-menu-impl/src/main/kotlin/com/evgeny/goncharov/multimodule/bottom_menu_impl/navigation/BottomMenuNavigator.kt
@@ -2,16 +2,22 @@ package com.evgeny.goncharov.multimodule.bottom_menu_impl.navigation
 
 import androidx.fragment.app.FragmentFactory
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
+import com.evgeny.goncharov.multimodule.bottom_menu_impl.R
 import com.evgeny.goncharov.multimodule.bottom_menu_impl.ui.BottomMenuContainerFragment
+import com.evgeny.goncharov.sample.multimodule.di_core.ContainerFeatureFragment
+import com.evgeny.goncharov.sample.multimodule.navigation.base.GlobalForward
 import com.github.terrakok.cicerone.Command
 import com.github.terrakok.cicerone.Navigator
+import com.github.terrakok.cicerone.androidx.FragmentScreen
 
 internal class BottomMenuNavigator(
     private val fragmentBottomContainer: BottomMenuContainerFragment
 ) : Navigator {
 
     private val fm: FragmentManager = fragmentBottomContainer.childFragmentManager
-    private val fc: FragmentFactory = fm.fragmentFactory
+    private val ff: FragmentFactory = fm.fragmentFactory
+    private val idContainer: Int = R.id.frm_bottom_menu_feature_container
 
     override fun applyCommands(commands: Array<out Command>) {
         fm.executePendingTransactions()
@@ -20,6 +26,32 @@ internal class BottomMenuNavigator(
                 applyCommand(command)
             } catch (e: RuntimeException) {
                 errorOnApplyCommand(e)
+            }
+        }
+    }
+
+    private fun applyCommand(command: Command) {
+        when (command) {
+            is GlobalForward -> forward(command)
+        }
+    }
+
+    private fun forward(command: GlobalForward) {
+        val fragmentScreen = command.screen
+        val featureContainerFragment = fragmentScreen.createFragment(ff) as ContainerFeatureFragment
+        commitFragmentTransaction(featureContainerFragment, fragmentScreen, true)
+    }
+
+    private fun commitFragmentTransaction(
+        featureContainerFragment: ContainerFeatureFragment,
+        fragmentScreen: FragmentScreen,
+        addToBackStack: Boolean
+    ) {
+        fm.commit {
+            setReorderingAllowed(true)
+            replace(idContainer, featureContainerFragment, fragmentScreen.screenKey)
+            if (addToBackStack) {
+                addToBackStack(featureContainerFragment.backStackName)
             }
         }
     }
