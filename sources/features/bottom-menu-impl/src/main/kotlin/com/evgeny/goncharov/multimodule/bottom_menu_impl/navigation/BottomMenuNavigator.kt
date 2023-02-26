c2b7e95b324722c81b284ff7e77155f2a36a68ca
Evgeny Goncharov <jtgn@yandex.ru>
2023-02-26 10:01:41 +0300

    refactor BottomMenuNavigator

--- sources/features/bottom-menu-impl/src/main/kotlin/com/evgeny/goncharov/multimodule/bottom_menu_impl/navigation/BottomMenuNavigator.kt
+++ sources/features/bottom-menu-impl/src/main/kotlin/com/evgeny/goncharov/multimodule/bottom_menu_impl/navigation/BottomMenuNavigator.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.commit
 import com.evgeny.goncharov.multimodule.bottom_menu_impl.R
 import com.evgeny.goncharov.multimodule.bottom_menu_impl.ui.BottomMenuContainerFragment
 import com.evgeny.goncharov.sample.multimodule.di_core.ContainerFeatureFragment
+import com.evgeny.goncharov.sample.multimodule.navigation.base.BaseNavigator
 import com.evgeny.goncharov.sample.multimodule.navigation.commands.GlobalForward
 import com.github.terrakok.cicerone.Command
 import com.github.terrakok.cicerone.Navigator
@@ -13,25 +14,14 @@ import com.github.terrakok.cicerone.androidx.FragmentScreen
 
 internal class BottomMenuNavigator(
     private val fragmentBottomContainer: BottomMenuContainerFragment
-) : Navigator {
+) : BaseNavigator() {
 
-    private val fm: FragmentManager = fragmentBottomContainer.childFragmentManager
-    private val ff: FragmentFactory = fm.fragmentFactory
-    private val idContainer: Int = R.id.frm_bottom_menu_feature_container
+    override val fm: FragmentManager = fragmentBottomContainer.childFragmentManager
+    override val ff: FragmentFactory = fm.fragmentFactory
+    override val containerId: Int = R.id.frm_bottom_menu_feature_container
     private var selectedBackstackMenu = ""
 
-    override fun applyCommands(commands: Array<out Command>) {
-        fm.executePendingTransactions()
-        for (command in commands) {
-            try {
-                applyCommand(command)
-            } catch (e: RuntimeException) {
-                errorOnApplyCommand(e)
-            }
-        }
-    }
-
-    private fun applyCommand(command: Command) {
+    override fun applyCommand(command: Command) {
         when (command) {
             is GlobalForward -> forward(command)
         }
@@ -42,37 +32,7 @@ internal class BottomMenuNavigator(
         val featureContainerFragment = fragmentScreen.createFragment(ff) as ContainerFeatureFragment
         if (selectedBackstackMenu != featureContainerFragment.backStackName) {
             commitFragmentTransaction(featureContainerFragment, fragmentScreen)
-        }
-    }
-
-    private fun commitFragmentTransaction(
-        featureContainerFragment: ContainerFeatureFragment,
-        fragmentScreen: FragmentScreen,
-    ) {
-        fm.commit {
-            setReorderingAllowed(true)
-            replace(idContainer, featureContainerFragment, fragmentScreen.screenKey)
-            addToBackStack(featureContainerFragment.backStackName)
             selectedBackstackMenu = featureContainerFragment.backStackName
         }
-    }
-
-    private fun errorOnApplyCommand(
-        error: RuntimeException
-    ) {
-        error(
-            ERROR_MESSAGE.format(
-                fragmentBottomContainer.javaClass.canonicalName,
-                TAG,
-                error.message
-            )
-        )
-    }
-
-    private companion object {
-
-        const val TAG = "BottomMenuContainerFragment"
-        const val ERROR_MESSAGE =
-            "errorOnApplyCommand host: %s tag: %s error message: %s"
     }
 }
