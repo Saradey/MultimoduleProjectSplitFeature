68b8ab68f9bcf9fe13419b9e685a2cf4ffb822d8
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-03 23:55:54 +0300

    added analytics module

---
+++ sources/core/analytics/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.evgeny.goncharov.sample.multimodule.analytics"
+    compileSdk = rootProject.ext["compileSdkVersionApp"] as Int
+    defaultConfig {
+        minSdk = rootProject.ext["minSdkVersionApp"] as Int
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-Xexplicit-api=strict")
+    }
+}
+
+dependencies {
+
+}
