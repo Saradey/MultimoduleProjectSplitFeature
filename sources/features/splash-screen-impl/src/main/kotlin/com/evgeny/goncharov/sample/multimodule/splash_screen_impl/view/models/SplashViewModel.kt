e6aeedddc8469b7a18b42dbab21680189866b56e
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-04 23:01:45 +0300

    added dependency to SplashViewModel

--- sources/features/splash-screen-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/splash_screen_impl/view/models/SplashViewModel.kt
+++ sources/features/splash-screen-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/splash_screen_impl/view/models/SplashViewModel.kt
@@ -2,13 +2,18 @@ package com.evgeny.goncharov.sample.multimodule.splash_screen_impl.view.models
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.evgeny.goncharov.sample.multimodule.analytics.api.AnalyticsDependencyApi
 import com.evgeny.goncharov.sample.multimodule.bottom_menu_api.BottomMenuLauncher
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 internal class SplashViewModel @Inject constructor(
+    private val mainLauncher: BottomMenuLauncher,
+    private val analyticsDependencyApi: AnalyticsDependencyApi
 ) : ViewModel() {
+
+
 
     companion object {
 
