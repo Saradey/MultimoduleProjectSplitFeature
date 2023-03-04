5d487188e2f12eac53e881860c65e2d138f4a81a
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-04 21:52:16 +0300

    add logic AnalyticsFirebaseManagerImpl

--- sources/core/analytics/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/analytics/manager/AnalyticsFirebaseManagerImpl.kt
+++ sources/core/analytics/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/analytics/manager/AnalyticsFirebaseManagerImpl.kt
@@ -1,8 +1,14 @@
 package com.evgeny.goncharov.sample.multimodule.analytics.manager
 
+import android.os.Bundle
+import com.evgeny.goncharov.sample.multimodule.analytics.firebase.FirebaseAnalytics
 import javax.inject.Inject
 
 internal class AnalyticsFirebaseManagerImpl @Inject constructor(
-
+    private val firebaseAnalytics: FirebaseAnalytics
 ) : AnalyticsManager {
+
+    fun logEvent(key: String, eventBundle: Bundle) {
+        firebaseAnalytics.logEvent(key, eventBundle)
+    }
 }
