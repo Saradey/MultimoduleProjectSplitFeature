47a87fab530ecf7df8c3c3d13c1857ac0ee1b91f
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-05 17:57:34 +0300

    added HiAnalytics to graph

--- sources/core/analytics/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/analytics/modules/AnalyticsProvidesModule.kt
+++ sources/core/analytics/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/analytics/modules/AnalyticsProvidesModule.kt
@@ -2,6 +2,7 @@ package com.evgeny.goncharov.sample.multimodule.analytics.modules
 
 import android.content.Context
 import com.evgeny.goncharov.sample.multimodule.analytics.firebase.FirebaseAnalytics
+import com.evgeny.goncharov.sample.multimodule.analytics.huawei.HiAnalytics
 import com.evgeny.goncharov.sample.multimodule.di_core.scopes.CoreScope
 import dagger.Module
 import dagger.Provides
@@ -13,5 +14,11 @@ internal object AnalyticsProvidesModule {
     @Provides
     fun provideAnalyticsManager(context: Context): FirebaseAnalytics {
         return FirebaseAnalytics.getInstance(context)
+    }
+
+    @CoreScope
+    @Provides
+    fun provideHiAnalytics(context: Context): HiAnalytics {
+        return HiAnalytics.getInstance(context)
     }
 }
