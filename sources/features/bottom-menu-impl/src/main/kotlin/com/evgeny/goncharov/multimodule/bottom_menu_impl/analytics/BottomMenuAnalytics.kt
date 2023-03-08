d6ff7ed7fb9d1333f626092314022adce65f6b51
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-08 23:30:22 +0300

    added goToTheHome

--- sources/features/bottom-menu-impl/src/main/kotlin/com/evgeny/goncharov/multimodule/bottom_menu_impl/analytics/BottomMenuAnalytics.kt
+++ sources/features/bottom-menu-impl/src/main/kotlin/com/evgeny/goncharov/multimodule/bottom_menu_impl/analytics/BottomMenuAnalytics.kt
@@ -19,7 +19,8 @@ internal class BottomMenuAnalytics @Inject constructor(
     }
 
     fun goToTheHome() {
-
+        params.add(UI_EVENT_GO_TO_HOME to null)
+        analyticsDependencyApi.logEvent(UI_EVENT_KEY_NAVIGATION, params)
     }
 
     fun goToTheCatalog() {
@@ -41,5 +42,6 @@ internal class BottomMenuAnalytics @Inject constructor(
     private companion object {
         const val TAG_BOTTOM_MENU_PARAM = "bottom_menu"
         const val UI_EVENT_START_HOME = "bottom_menu_to_start_home"
+        const val UI_EVENT_GO_TO_HOME = "bottom_menu_go_to_home"
     }
 }
