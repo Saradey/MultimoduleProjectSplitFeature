06941f7457956c24956792b90cf6eb723ea52396
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-09 00:37:48 +0300

    added MainActivityViewModel to dagger graph main activity

--- sources/engine/main-activity/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/di/modules/MainActivityViewModelModule.kt
+++ sources/engine/main-activity/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/di/modules/MainActivityViewModelModule.kt
@@ -1,13 +1,20 @@
 package com.evgeny.goncharov.sample.multimodule.di.modules
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.evgeny.goncharov.sample.multimodule.di_core.key.ViewModelKey
 import com.evgeny.goncharov.sample.multimodule.di_core.scopes.FeatureScope
 import com.evgeny.goncharov.sample.multimodule.reusable_sources.MultiViewModelFactory
+import com.evgeny.goncharov.sample.multimodule.view.models.MainActivityViewModel
 import dagger.Binds
+import dagger.multibindings.IntoMap
 
 internal interface MainActivityViewModelModule {
 
     @[Binds FeatureScope]
     fun MultiViewModelFactory.provideMultiViewModelFactory(): ViewModelProvider.Factory
 
+    @[Binds FeatureScope]
+    @[IntoMap ViewModelKey(MainActivityViewModel::class)]
+    fun MainActivityViewModel.provideMainActivityViewModel(): ViewModel
 }
