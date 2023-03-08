33431e529a1fb48c4d7feef03c9a471c982197df
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-09 00:22:15 +0300

    fix SplashAnalytics

--- sources/features/splash-screen-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/splash_screen_impl/analytics/SplashAnalytics.kt
+++ sources/features/splash-screen-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/splash_screen_impl/analytics/SplashAnalytics.kt
@@ -9,18 +9,23 @@ internal class SplashAnalytics @Inject constructor(
     private val analyticsDependencyApi: AnalyticsManager
 ) {
 
-    private val params: MutableList<Pair<String, String?>>
-        get() = mutableListOf<Pair<String, String?>>(
-            TAG_SCREEN_PARAM to TAG_SPLASH_SCREEN_PARAM
-        )
+    private val params: MutableList<Pair<String, String?>> = mutableListOf<Pair<String, String?>>(
+        TAG_SCREEN_PARAM to TAG_SPLASH_PARAM
+    )
+
+    private fun prepare() {
+        params.clear()
+        params.add(TAG_SCREEN_PARAM to TAG_SPLASH_PARAM)
+    }
 
     fun goToTheBottomMainMenu() {
+        prepare()
         params.add(UI_EVENT_GO_BOTTOM_MENU to null)
         analyticsDependencyApi.logEvent(UI_EVENT_KEY_NAVIGATION, params)
     }
 
     private companion object {
-        const val TAG_SPLASH_SCREEN_PARAM = "splash"
+        const val TAG_SPLASH_PARAM = "splash"
         const val UI_EVENT_GO_BOTTOM_MENU = "splash_go_to_bottom_menu"
     }
 }
