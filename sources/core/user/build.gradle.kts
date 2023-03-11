04ea50322870dd76590abc7ac35513fbc94a3201
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-11 11:22:32 +0300

    config user gradle module

--- sources/core/user/build.gradle.kts
+++ sources/core/user/build.gradle.kts
@@ -1,28 +1,14 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
     namespace = "com.evgeny.goncharov.sample.multimodule.user"
-    compileSdk = 33
-
+    compileSdk = rootProject.ext["compileSdkVersionApp"] as Int
     defaultConfig {
-        minSdk = 24
-        targetSdk = 33
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+        minSdk = rootProject.ext["minSdkVersionApp"] as Int
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -34,11 +20,7 @@ android {
 }
 
 dependencies {
+    implementation(coreLibs.dagger)
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    kapt(coreLibs.daggerCompiler)
 }
