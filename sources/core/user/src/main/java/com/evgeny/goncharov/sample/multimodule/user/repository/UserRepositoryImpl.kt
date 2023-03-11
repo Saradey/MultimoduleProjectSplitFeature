e972b169cbef4b27818ce1c5621bb1620849c222
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-11 12:10:00 +0300

    added dependency

--- sources/core/user/src/main/java/com/evgeny/goncharov/sample/multimodule/user/repository/UserRepositoryImpl.kt
+++ sources/core/user/src/main/java/com/evgeny/goncharov/sample/multimodule/user/repository/UserRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.evgeny.goncharov.sample.multimodule.user.repository
 
+import android.content.Context
 import javax.inject.Inject
 
-internal class UserRepositoryImpl @Inject constructor() : UserRepository {
-}
+internal class UserRepositoryImpl @Inject constructor(
+    private val applicationContext: Context
+) : UserRepository {}
--- sources/core/user/src/main/java/com/evgeny/goncharov/sample/multimodule/user/usecase/UserUseCaseImpl.kt
+++ sources/core/user/src/main/java/com/evgeny/goncharov/sample/multimodule/user/usecase/UserUseCaseImpl.kt
@@ -1,6 +1,9 @@
 package com.evgeny.goncharov.sample.multimodule.user.usecase
 
+import com.evgeny.goncharov.sample.multimodule.user.repository.UserRepository
 import javax.inject.Inject
 
-internal class UserUseCaseImpl @Inject constructor() : UserUseCase {
+internal class UserUseCaseImpl @Inject constructor(
+    private val userRepository: UserRepository
+) : UserUseCase {
 }
