231eb07c45157dd7c191e73e8e0367630d580694
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-12 14:51:25 +0300

    refactor

--- sources/features/authorization-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/authorization_impl/ui/RegistrationFragment.kt
+++ sources/features/authorization-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/authorization_impl/ui/RegistrationFragment.kt
@@ -5,7 +5,7 @@ import android.view.View
 import com.evgeny.goncharov.sample.multimodule.di_core.CoreFragment
 import com.evgeny.goncharov.sample.multimodule.authorization_impl.R
 
-internal class RegistrationFragment : CoreFragment(R.layout.fragment_registration) {
+internal class RegistrationFragment : CoreFragment(R.layout.fragment_authorization) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     }
