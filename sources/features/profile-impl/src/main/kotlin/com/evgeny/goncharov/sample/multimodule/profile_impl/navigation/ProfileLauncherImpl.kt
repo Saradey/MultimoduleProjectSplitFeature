5a4f7374d2e5f208df9890bb3006e6a753dd3be2
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-16 23:33:26 +0300

    added profile launcher

---
+++ sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/navigation/ProfileLauncherImpl.kt
@@ -0,0 +1,10 @@
+package com.evgeny.goncharov.sample.multimodule.profile_impl.navigation
+
+import com.evgeny.goncharov.sample.multimodule.profile_api.ProfileLauncher
+
+public class ProfileLauncherImpl : ProfileLauncher {
+
+    override fun launch() {
+
+    }
+}
---
+++ sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/navigation/ProfileScreens.kt
@@ -0,0 +1,4 @@
+package com.evgeny.goncharov.sample.multimodule.profile_impl.navigation
+
+internal object ProfileScreens {
+}
