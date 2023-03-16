6ca8c6d84023ce51915fdba7a5cb0a88fc9b567e
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-16 23:59:51 +0300

    added navigator to ProfileContainerFragment

--- sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/ui/ProfileContainerFragment.kt
+++ sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/ui/ProfileContainerFragment.kt
@@ -1,6 +1,8 @@
 package com.evgeny.goncharov.sample.multimodule.profile_impl.ui
 
 import com.evgeny.goncharov.sample.multimodule.di_core.ContainerFeatureFragment
+import com.evgeny.goncharov.sample.multimodule.navigation.base.FeatureNavigator
+import com.evgeny.goncharov.sample.multimodule.navigation.routers.FeatureRouter
 import com.evgeny.goncharov.sample.multimodule.profile_api.ProfileApi
 import com.evgeny.goncharov.sample.multimodule.profile_impl.di.contracts.ProfileInternal
 import com.github.terrakok.cicerone.Navigator
@@ -11,11 +13,11 @@ internal class ProfileContainerFragment : ContainerFeatureFragment() {
     private val dependency: ProfileInternal by lazy {
         getFeatureApi(ProfileApi::class.java) as ProfileInternal
     }
-
-    override val navigatorHolder: NavigatorHolder
-        get() = TODO("Not yet implemented")
-    override val navigator: Navigator
-        get() = TODO("Not yet implemented")
+    override val navigator: FeatureNavigator by lazy {
+        FeatureNavigator(this)
+    }
+    private val router: FeatureRouter = dependency.provideFeatureRouter()
+    override val navigatorHolder: NavigatorHolder = dependency.provideFeatureNavigatorHolder()
 
     override fun releaseDependencies() {
         releaseFeatureApi(ProfileApi::class.java)
