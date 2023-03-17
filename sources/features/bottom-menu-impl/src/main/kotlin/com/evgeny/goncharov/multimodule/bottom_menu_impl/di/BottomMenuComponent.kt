a906f6242598bb10ace28eaf69dddd13939a710b
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-17 22:11:40 +0300

    added ProfileLauncher to dagger graph bottom menu

--- sources/features/bottom-menu-impl/src/main/kotlin/com/evgeny/goncharov/multimodule/bottom_menu_impl/di/BottomMenuComponent.kt
+++ sources/features/bottom-menu-impl/src/main/kotlin/com/evgeny/goncharov/multimodule/bottom_menu_impl/di/BottomMenuComponent.kt
@@ -9,6 +9,7 @@ import com.evgeny.goncharov.sample.multimodule.home_api.HomeLauncher
 import com.evgeny.goncharov.sample.multimodule.like_api.LikeLauncher
 import com.evgeny.goncharov.sample.multimodule.navigation.api.CoreNavigationApi
 import com.evgeny.goncharov.sample.multimodule.authorization_api.AuthorizationLauncher
+import com.evgeny.goncharov.sample.multimodule.profile_api.ProfileLauncher
 import com.evgeny.goncharov.sample.multimodule.user.di.api.UserApi
 import dagger.BindsInstance
 import dagger.Component
@@ -36,7 +37,8 @@ internal interface BottomMenuComponent : BottomMenuInternal {
             @BindsInstance homeLauncher: HomeLauncher,
             @BindsInstance catalogLauncher: CatalogLauncher,
             @BindsInstance authorizationLauncher: AuthorizationLauncher,
-            @BindsInstance likeLauncher: LikeLauncher
+            @BindsInstance likeLauncher: LikeLauncher,
+            @BindsInstance profileLauncher: ProfileLauncher
         ): BottomMenuComponent
     }
 }
--- sources/features/bottom-menu-impl/src/main/kotlin/com/evgeny/goncharov/multimodule/bottom_menu_impl/di/holders/BottomMenuHolder.kt
+++ sources/features/bottom-menu-impl/src/main/kotlin/com/evgeny/goncharov/multimodule/bottom_menu_impl/di/holders/BottomMenuHolder.kt
@@ -10,6 +10,7 @@ import com.evgeny.goncharov.sample.multimodule.home_api.HomeLauncher
 import com.evgeny.goncharov.sample.multimodule.like_api.LikeLauncher
 import com.evgeny.goncharov.sample.multimodule.navigation.api.CoreNavigationApi
 import com.evgeny.goncharov.sample.multimodule.authorization_api.AuthorizationLauncher
+import com.evgeny.goncharov.sample.multimodule.profile_api.ProfileLauncher
 import com.evgeny.goncharov.sample.multimodule.user.di.api.UserApi
 
 public class BottomMenuHolder(container: FeatureContainer) :
@@ -24,7 +25,8 @@ public class BottomMenuHolder(container: FeatureContainer) :
                 getFeatureLauncher(HomeLauncher::class.java),
                 getFeatureLauncher(CatalogLauncher::class.java),
                 getFeatureLauncher(AuthorizationLauncher::class.java),
-                getFeatureLauncher(LikeLauncher::class.java)
+                getFeatureLauncher(LikeLauncher::class.java),
+                getFeatureLauncher(ProfileLauncher::class.java)
             )
     }
 }
