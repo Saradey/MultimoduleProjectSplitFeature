030fa459025f4b2a5046de5a000a3e4d87d7607b
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-17 21:16:44 +0300

    added viewModel to AuthorizationFragment

--- sources/features/authorization-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/authorization_impl/ui/AuthorizationFragment.kt
+++ sources/features/authorization-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/authorization_impl/ui/AuthorizationFragment.kt
@@ -2,10 +2,22 @@ package com.evgeny.goncharov.sample.multimodule.authorization_impl.ui
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import com.evgeny.goncharov.sample.multimodule.authorization_api.AuthorizationApi
 import com.evgeny.goncharov.sample.multimodule.di_core.CoreFragment
 import com.evgeny.goncharov.sample.multimodule.authorization_impl.R
+import com.evgeny.goncharov.sample.multimodule.authorization_impl.di.contracts.AuthorizationInternal
+import com.evgeny.goncharov.sample.multimodule.authorization_impl.view.models.AuthorizationViewModel
 
 internal class AuthorizationFragment : CoreFragment(R.layout.fragment_authorization) {
+
+    private val dependency: AuthorizationInternal by lazy {
+        getFeatureApi(AuthorizationApi::class.java) as AuthorizationInternal
+    }
+
+    private val viewModel: AuthorizationViewModel by viewModels {
+        dependency.provideViewModelFactory()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     }
