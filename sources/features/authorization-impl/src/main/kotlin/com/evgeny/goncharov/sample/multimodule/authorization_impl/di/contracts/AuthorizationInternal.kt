c05f5117e8b2799e29c0c67e3397a572feb6d9e6
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-17 21:39:02 +0300

    fix dagger crash

--- sources/features/authorization-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/authorization_impl/di/contracts/AuthorizationInternal.kt
+++ sources/features/authorization-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/authorization_impl/di/contracts/AuthorizationInternal.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.evgeny.goncharov.sample.multimodule.navigation.di.FeatureNavigationContract
 import com.evgeny.goncharov.sample.multimodule.authorization_api.AuthorizationApi
 
-internal interface AuthorizationInternal : AuthorizationApi, FeatureNavigationContract{
+internal interface AuthorizationInternal : AuthorizationApi, FeatureNavigationContract {
 
     fun provideViewModelFactory(): ViewModelProvider.Factory
 }
--- sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/di/ProfileComponent.kt
+++ sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/di/ProfileComponent.kt
@@ -1,9 +1,11 @@
 package com.evgeny.goncharov.sample.multimodule.profile_impl.di
 
+import com.evgeny.goncharov.sample.multimodule.di_core.scopes.FeatureScope
 import com.evgeny.goncharov.sample.multimodule.navigation.di.FeatureNavigationModule
 import com.evgeny.goncharov.sample.multimodule.profile_impl.di.contracts.ProfileInternal
 import dagger.Component
 
+@FeatureScope
 @Component(modules = [FeatureNavigationModule::class])
 internal interface ProfileComponent : ProfileInternal {
 
