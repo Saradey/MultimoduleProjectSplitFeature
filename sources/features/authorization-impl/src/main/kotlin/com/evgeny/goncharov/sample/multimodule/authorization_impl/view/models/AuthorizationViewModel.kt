3a11440fc7efdfa0695e7c2e2bc887e69301dec3
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-17 21:43:03 +0300

    added UserUseCase to AuthorizationViewModel

--- sources/features/authorization-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/authorization_impl/view/models/AuthorizationViewModel.kt
+++ sources/features/authorization-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/authorization_impl/view/models/AuthorizationViewModel.kt
@@ -1,9 +1,12 @@
 package com.evgeny.goncharov.sample.multimodule.authorization_impl.view.models
 
 import androidx.lifecycle.ViewModel
+import com.evgeny.goncharov.sample.multimodule.user.usecase.UserUseCase
 import javax.inject.Inject
 
-internal class AuthorizationViewModel @Inject constructor() : ViewModel() {
+internal class AuthorizationViewModel @Inject constructor(
+    private val useCase: UserUseCase
+) : ViewModel() {
 
 
 }
