19c3caaa5b44e467a1f314ea23dff6e481adc48f
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-18 19:34:59 +0300

    added dependency to profile module

--- sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/di/ProfileComponent.kt
+++ sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/di/ProfileComponent.kt
@@ -1,9 +1,12 @@
 package com.evgeny.goncharov.sample.multimodule.profile_impl.di
 
+import com.evgeny.goncharov.sample.multimodule.authorization_api.AuthorizationLauncher
 import com.evgeny.goncharov.sample.multimodule.di_core.scopes.FeatureScope
 import com.evgeny.goncharov.sample.multimodule.navigation.di.FeatureNavigationModule
 import com.evgeny.goncharov.sample.multimodule.profile_impl.di.contracts.ProfileInternal
 import com.evgeny.goncharov.sample.multimodule.profile_impl.di.modules.ViewModelsProfileModule
+import com.evgeny.goncharov.sample.multimodule.user.di.api.UserApi
+import dagger.BindsInstance
 import dagger.Component
 
 @FeatureScope
@@ -11,14 +14,19 @@ import dagger.Component
     modules = [
         FeatureNavigationModule::class,
         ViewModelsProfileModule::class
+    ],
+    dependencies = [
+        UserApi::class
     ]
 )
 internal interface ProfileComponent : ProfileInternal {
 
-
     @Component.Factory
     interface Factory {
 
-        fun create(): ProfileComponent
+        fun create(
+            userApi: UserApi,
+            @BindsInstance authorizationLauncher: AuthorizationLauncher
+        ): ProfileComponent
     }
 }
--- sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/di/holder/ProfileHolder.kt
+++ sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/di/holder/ProfileHolder.kt
@@ -1,14 +1,19 @@
 package com.evgeny.goncharov.sample.multimodule.profile_impl.di.holder
 
+import com.evgeny.goncharov.sample.multimodule.authorization_api.AuthorizationLauncher
 import com.evgeny.goncharov.sample.multimodule.di_core.container.FeatureContainer
 import com.evgeny.goncharov.sample.multimodule.di_core.holder.FeatureHolder
 import com.evgeny.goncharov.sample.multimodule.profile_api.ProfileApi
 import com.evgeny.goncharov.sample.multimodule.profile_impl.di.DaggerProfileComponent
+import com.evgeny.goncharov.sample.multimodule.user.di.api.UserApi
 
 public class ProfileHolder(container: FeatureContainer) : FeatureHolder<ProfileApi>(container) {
 
     override fun buildComponent(): ProfileApi {
         return DaggerProfileComponent.factory()
-            .create()
+            .create(
+                getGlobalComponent(UserApi::class.java),
+                getFeatureLauncher(AuthorizationLauncher::class.java)
+            )
     }
 }
