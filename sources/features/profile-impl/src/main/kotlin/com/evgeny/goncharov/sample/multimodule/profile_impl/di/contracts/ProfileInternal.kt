e2db5e8fd4207b4dd207ceaa700c7acc5ec76292
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-18 19:26:30 +0300

    added viewModel to ProfileFragment

--- sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/di/contracts/ProfileInternal.kt
+++ sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/di/contracts/ProfileInternal.kt
@@ -1,6 +1,10 @@
 package com.evgeny.goncharov.sample.multimodule.profile_impl.di.contracts
 
+import androidx.lifecycle.ViewModelProvider
 import com.evgeny.goncharov.sample.multimodule.navigation.di.FeatureNavigationContract
 import com.evgeny.goncharov.sample.multimodule.profile_api.ProfileApi
 
-internal interface ProfileInternal : ProfileApi, FeatureNavigationContract
+internal interface ProfileInternal : ProfileApi, FeatureNavigationContract {
+
+    fun provideViewModelFactory(): ViewModelProvider.Factory
+}
--- sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/ui/ProfileFragment.kt
+++ sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/ui/ProfileFragment.kt
@@ -2,10 +2,21 @@ package com.evgeny.goncharov.sample.multimodule.profile_impl.ui
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.evgeny.goncharov.sample.multimodule.di_core.CoreFragment
+import com.evgeny.goncharov.sample.multimodule.profile_api.ProfileApi
 import com.evgeny.goncharov.sample.multimodule.profile_impl.R
+import com.evgeny.goncharov.sample.multimodule.profile_impl.di.contracts.ProfileInternal
+import com.evgeny.goncharov.sample.multimodule.profile_impl.view.models.ProfileViewModel
 
-internal class ProfileFragment: CoreFragment(R.layout.fragment_profile) {
+internal class ProfileFragment : CoreFragment(R.layout.fragment_profile) {
+
+    private val dependency: ProfileInternal by lazy {
+        getFeatureApi(ProfileApi::class.java) as ProfileInternal
+    }
+    private val viewModel: ProfileViewModel by viewModels {
+        dependency.provideViewModelFactory()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     }
