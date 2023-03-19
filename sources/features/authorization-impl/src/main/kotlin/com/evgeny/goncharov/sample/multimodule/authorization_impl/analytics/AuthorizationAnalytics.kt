fd72882062bc7882058160952d6bbe58bfdf0f2e
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-19 14:24:32 +0300

    added empty AuthorizationAnalytics

---
+++ sources/features/authorization-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/authorization_impl/analytics/AuthorizationAnalytics.kt
@@ -0,0 +1,6 @@
+package com.evgeny.goncharov.sample.multimodule.authorization_impl.analytics
+
+import javax.inject.Inject
+
+internal class AuthorizationAnalytics @Inject constructor() {
+}
--- sources/features/authorization-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/authorization_impl/view/models/AuthorizationViewModel.kt
+++ sources/features/authorization-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/authorization_impl/view/models/AuthorizationViewModel.kt
@@ -1,13 +1,15 @@
 package com.evgeny.goncharov.sample.multimodule.authorization_impl.view.models
 
 import androidx.lifecycle.ViewModel
+import com.evgeny.goncharov.sample.multimodule.authorization_impl.analytics.AuthorizationAnalytics
 import com.evgeny.goncharov.sample.multimodule.profile_api.ProfileLauncher
 import com.evgeny.goncharov.sample.multimodule.user.usecase.UserUseCase
 import javax.inject.Inject
 
 internal class AuthorizationViewModel @Inject constructor(
     private val useCase: UserUseCase,
-    private val profileLauncher: ProfileLauncher
+    private val profileLauncher: ProfileLauncher,
+    private val analytics: AuthorizationAnalytics
 ) : ViewModel() {
 
     fun signIn() {
