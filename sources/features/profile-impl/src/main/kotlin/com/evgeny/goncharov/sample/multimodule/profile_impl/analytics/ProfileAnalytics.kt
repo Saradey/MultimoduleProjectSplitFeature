5d3a8c0cdc0b7b1829d3ee0c47235009ff837b8c
Evgeny Goncharov <jtgn@yandex.ru>
2023-03-19 14:17:04 +0300

    added logic ProfileAnalytics

--- sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/analytics/ProfileAnalytics.kt
+++ sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/analytics/ProfileAnalytics.kt
@@ -1,12 +1,28 @@
 package com.evgeny.goncharov.sample.multimodule.profile_impl.analytics
 
 import com.evgeny.goncharov.sample.multimodule.analytics.manager.AnalyticsManager
+import com.evgeny.goncharov.sample.multimodule.analytics.utils.NAVIGATION_EVENT_KEY
+import com.evgeny.goncharov.sample.multimodule.analytics.utils.TAG_SCREEN_PARAM
 import javax.inject.Inject
 
 internal class ProfileAnalytics @Inject constructor(
     private val analyticsManager: AnalyticsManager
 ) {
 
+    private val params: MutableList<Pair<String, String?>> = mutableListOf<Pair<String, String?>>(
+        TAG_SCREEN_PARAM to TAG_PROFILE_PARAM
+    )
+
+    private fun prepare() {
+        params.clear()
+        params.add(TAG_SCREEN_PARAM to TAG_PROFILE_PARAM)
+    }
+
+    fun goToAuth() {
+        prepare()
+        params.add(NAVIGATION_EVENT_GO_AUTH to null)
+        analyticsManager.logEvent(NAVIGATION_EVENT_KEY, params)
+    }
 
     private companion object {
 
--- sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/view/models/ProfileViewModel.kt
+++ sources/features/profile-impl/src/main/kotlin/com/evgeny/goncharov/sample/multimodule/profile_impl/view/models/ProfileViewModel.kt
@@ -2,16 +2,19 @@ package com.evgeny.goncharov.sample.multimodule.profile_impl.view.models
 
 import androidx.lifecycle.ViewModel
 import com.evgeny.goncharov.sample.multimodule.authorization_api.AuthorizationLauncher
+import com.evgeny.goncharov.sample.multimodule.profile_impl.analytics.ProfileAnalytics
 import com.evgeny.goncharov.sample.multimodule.user.usecase.UserUseCase
 import javax.inject.Inject
 
 internal class ProfileViewModel @Inject constructor(
     private val authorizationLauncher: AuthorizationLauncher,
-    private val userUseCase: UserUseCase
+    private val userUseCase: UserUseCase,
+    private val profileAnalytics: ProfileAnalytics
 ) : ViewModel() {
 
     fun signOut() {
         userUseCase.signOut()
         authorizationLauncher.launchReplace()
+        profileAnalytics.goToAuth()
     }
 }
